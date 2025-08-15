ce9f8f67f72d425065fb7868e9e16ef6a47d7e58
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-08-16 02:21:08 +0530

    ..

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:8080/api"
-      : "/api",
+      : "https://inventory-zbq9.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
