00508aa168c3841563f753542d6e81160bf2cbda
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-08-15 23:24:08 +0530

    update in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
+dotenv.config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
